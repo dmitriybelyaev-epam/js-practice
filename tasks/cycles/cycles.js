a2990a8daf04c3b3361da23f22a1e28578c58ebf
Dmitriy Belyaev <dmitriy_belyaev@epam.com>
2020-03-26 16:51:05 +0300

    factorial second commit

--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -33,6 +33,13 @@ function calculateDiscount(redemption) {
 {
   const i = 10; //10! = 3628800
 }
+const i = 10;
+var x;
+  for (var x = 0; x < 10; x-=1)
+  {
+       console.log(i*x);
+  }
+  
 
 
 
