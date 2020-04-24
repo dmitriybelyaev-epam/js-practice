7f3b2da76369b1781bf26f42a8f3a55621620cc9
Dmitriy Belyaev <dmitriy_belyaev@epam.com>
2020-04-24 13:22:14 +0300

    Update async.js
    Completed task on async.js.

--- tasks/async/async.js
+++ tasks/async/async.js
@@ -9,36 +9,52 @@ const {
  * console.log data got from callbackFunction
  */
 function printDataCallback() {
+    function printMe (err, info) {
+        console.log(info);
+    }
+    callbackFunction(printMe);
 }
 
 /**
  * console.log data got from promiseFunction
  */
 function printDataPromise() {
+    promiseFunction().then(data => {
+        console.log(data);
+    });
 }
 
 /**
  * console.log data got from promiseFunction using async/await
  */
 async function printDataAsyncAwait() {
+    let data = await promiseFunction();
+    console.log(data);
 }
 
 /**
  * throw error data from callbackFunctionError
  */
 function handleErrorCallback() {
-}
+    function errorData(error) {
+        throw error;
+    }
+        callbackFunctionError(errorData);
+    }
 
 /**
  * throw error come from promiseFunctionError
  */
 function handlePromiseError() {
+    promiseFunctionError.catch(err => alert(err));
 }
 
 /**
  * throw error come from promiseFunctionError using async/await
  */
 async function handleAsyncAwaitError() {
+    const error = await promiseFunctionError();
+    alert(error);
 }
 
 module.exports = {
